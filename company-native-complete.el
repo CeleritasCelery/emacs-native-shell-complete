9a734db7520d14ccdd4779ad641d655983fef6dd
Troy Hinckley <t.macman@gmail.com>
2020-03-15 15:44:04 -0600

    Add license boilerplate

--- company-native-complete.el
+++ company-native-complete.el
@@ -7,6 +7,20 @@
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "25.1")(company "0.9.0")(native-complete "0.1.0"))
 
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; This package provides a company backend for use with native-complete.
--- native-complete.el
+++ native-complete.el
@@ -7,6 +7,20 @@
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "25.1"))
 
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; This package interacts with a shell's native completion functionality to
